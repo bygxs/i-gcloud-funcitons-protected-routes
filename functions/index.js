5e7cf2518d33b70acefc58672087da3db0f61afa
bygxs <biniyamyohannes@icloud.com>
2025-03-04 13:11:26 +0100

    functions/index.js updated

--- functions/index.js
+++ functions/index.js
@@ -7,8 +7,8 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+//const {onRequest} = require("firebase-functions/v2/https");
+//const logger = require("firebase-functions/logger");
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
@@ -17,3 +17,32 @@ const logger = require("firebase-functions/logger");
 //   logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+// functions/index.js
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+admin.initializeApp();
+
+exports.verifyAuth = functions.https.onRequest(async (req, res) => {
+  if (req.method !== 'GET') {
+    return res.status(405).send('Method Not Allowed');
+  }
+
+  const authorizationHeader = req.headers.authorization;
+
+  if (!authorizationHeader || !authorizationHeader.startsWith('Bearer ')) {
+    return res.status(401).send('Unauthorized');
+  }
+
+  const token = authorizationHeader.split('Bearer ')[1];
+
+  try {
+    const decodedToken = await admin.auth().verifyIdToken(token);
+    // Token is valid.
+    return res.status(200).send({ message: 'Authenticated', uid: decodedToken.uid });
+  } catch (error) {
+    // Token is invalid.
+    return res.status(401).send('Unauthorized');
+  }
+});
